f17cfe03b641d31c872f9d74c03dc6773c165d33
andrei-mushovets <andreimyshoves89@gmail.com>
2025-12-24 10:22:39 +0100

    second lesson

--- server/middleware/ErrorHandlingMiddleware.js
+++ server/middleware/ErrorHandlingMiddleware.js
@@ -1,8 +1,8 @@
-const ApiError = require('../error/ApiError');
+const ApiError = require("../error/ApiError");
 
-export default function (err, req, res, next) {
+module.exports = function (err, req, res, next) {
   if (err instanceof ApiError) {
     return res.status(err.status).json({ message: err.message });
   }
   return res.status(500).json({ message: "Непредвиденная ошибка!" });
-}
+};
